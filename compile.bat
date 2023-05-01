ab8e194b7bb7778e60a2b44a58cd3f3e9d3d8919
L <dr.trayaurus61@gmail.com>
2023-04-30 19:32:22 -0700

    initial commit

---
+++ compile.bat
@@ -0,0 +1,4 @@
+make us2
+mv spm-rel-loader.us2 mod.rel
+mv mod.rel C:\Users\docaa\Documents\Mario\extracted\files\mod
+start "" "C:\Users\docaa\Documents\Mario\compile.bat"
---
+++ source/mod.cpp
@@ -0,0 +1,48 @@
+#include "mod.h"
+#include "patch.h"
+
+#include <spm/camdrv.h>
+#include <spm/fontmgr.h>
+#include <spm/seqdrv.h>
+#include <spm/seqdef.h>
+#include <wii/os/OSError.h>
+#include <wii/gx.h>
+
+namespace mod {
+/*
+    Title Screen Custom Text
+    Prints "SPM RPG Battles" at the top of the title screen
+*/
+
+static spm::seqdef::SeqFunc *seq_titleMainReal;
+static void seq_titleMainOverride(spm::seqdrv::SeqWork *wp)
+{
+    wii::gx::GXColor green = {0, 255, 0, 255};
+    f32 scale = 0.8f;
+    const char * msg = "SPM RPG Battles";
+    spm::fontmgr::FontDrawStart();
+    spm::fontmgr::FontDrawEdge();
+    spm::fontmgr::FontDrawColor(&green);
+    spm::fontmgr::FontDrawScale(scale);
+    spm::fontmgr::FontDrawNoiseOff();
+    spm::fontmgr::FontDrawRainbowColorOff();
+    f32 x = -((spm::fontmgr::FontGetMessageWidth(msg) * scale) / 2);
+    spm::fontmgr::FontDrawString(x, 200.0f, msg);
+    seq_titleMainReal(wp);
+}
+static void titleScreenCustomTextPatch()
+{
+    seq_titleMainReal = spm::seqdef::seq_data[spm::seqdrv::SEQ_TITLE].main;
+    spm::seqdef::seq_data[spm::seqdrv::SEQ_TITLE].main = &seq_titleMainOverride;
+}
+
+/*
+    General mod functions
+*/
+void main()
+{
+    wii::os::OSReport("SPM Rel Loader: the mod has ran!\n");
+//other functions
+}
+
+}
