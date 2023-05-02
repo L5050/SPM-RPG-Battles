b1cbaa644c8b75e8f2c97a6a6c4233d064ce36d5
L <dr.trayaurus61@gmail.com>
2023-05-01 21:17:10 -0700

    added evt_fade_entry

--- spm-headers/include/spm/evt_fade.h
+++ spm-headers/include/spm/evt_fade.h
@@ -9,7 +9,8 @@ UNKNOWN_FUNCTION(evt_fade_entry);
 
 // evt_fade_end_wait(s32)
 EVT_DECLARE_USER_FUNC(evt_fade_end_wait, 1)
-
+//evt_fade_entry(int transitionType, everything else is unknown ints)
+EVT_DECLARE_USER_FUNC(evt_fade_entry, 6)
 UNKNOWN_FUNCTION(func_800e715c);
 UNKNOWN_FUNCTION(func_800e71dc);
 UNKNOWN_FUNCTION(func_800e720c);
