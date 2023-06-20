b1a5fe49c1094201977cf6414e9252e9cf9e53fd
L <dr.trayaurus61@gmail.com>
2023-06-19 20:08:45 -0700

    linked a couple more events

--- spm-headers/include/spm/iValues.h
+++ spm-headers/include/spm/iValues.h
@@ -6,6 +6,10 @@ namespace spm::iValues {
 extern "C" {
 
 extern spm::evtmgr::EvtScriptCode * finalRpgChild;
+extern spm::evtmgr::EvtScriptCode * underchompAttack1;
+extern spm::evtmgr::EvtScriptCode * underchompAttack2;
+extern spm::evtmgr::EvtScriptCode * underchompAttack3;
+extern spm::evtmgr::EvtScriptCode * checkWinOrContinue;
 
 }
 }
